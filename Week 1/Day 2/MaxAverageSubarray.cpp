6aacdd03780aa9c95611588976c982d95f13af49
Vansh yadav <vanshyadav5444@gmail.com>
2026-06-05 12:55:12 +0530

    Create MaxAverageSubarray.cpp

---
+++ Week 1/Day 2/MaxAverageSubarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        //sliding window problem 
+        int sum=0;
+        double res;
+        for(int i=0; i<k;i++) sum+= nums[i];
+        res=sum;
+        for(int i= k; i<nums.size();i++){
+            sum = sum - nums[i-k];
+            sum=sum + nums[i];
+            if(sum>res) res = sum;
+        }
+        return res/k;
+    }
+};
