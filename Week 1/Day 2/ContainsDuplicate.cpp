f5623be8d7fd62bceda66be31601935b62dffc68
Vansh yadav <vanshyadav5444@gmail.com>
2026-06-05 12:53:55 +0530

    Create ContainsDuplicate.cpp

---
+++ Week 1/Day 2/ContainsDuplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> numSet;
+        for(int n :nums){
+            if(numSet.find(n)!=numSet.end()){
+                return true;
+            }
+            numSet.insert(n);
+        }
+        return false;
+        
+    }
+};
