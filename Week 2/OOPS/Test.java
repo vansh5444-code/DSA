e12cbc4de4ac00a78b9e885cc7c2998015fd42bb
Vansh yadav <vanshyadav5444@gmail.com>
2026-06-13 00:36:12 +0530

    Create Test.java

---
+++ Week 2/OOPS/Test.java
@@ -0,0 +1,25 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Veena v = new Veena();
+        v.play();
+
+        Saxophone s = new Saxophone();
+        s.play();
+
+        Playable instrument;
+
+        instrument = v;
+        instrument.play();
+
+        instrument = s;
+        instrument.play();
+    }
+}
