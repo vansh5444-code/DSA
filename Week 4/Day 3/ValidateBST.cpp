176929bf2202bf83ca633944f2bc22f2c46cc5c1
Vansh yadav <vanshyadav5444@gmail.com>
2026-06-24 22:19:59 +0530

    Create ValidateBST.cpp

---
+++ Week 4/Day 3/ValidateBST.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    bool isBST(TreeNode* root, long min, long max) {
+
+        if (root == NULL) {
+            return true;
+        }
+
+        if (root->val > min && root->val < max) {
+
+            bool left = isBST(root->left, min, root->val);
+            bool right = isBST(root->right, root->val, max);
+
+            return left && right;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return isBST(root, LONG_MIN, LONG_MAX);
+    }
+};
